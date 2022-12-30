91f4608079bd43eb17119e0d8201dacbdc081de1
Kayleigh Traister <kayleigh.traister@gmail.com>
2022-12-30 04:09:20 +0000

    more work on DH

--- endless_waltz/common/dh.go
+++ endless_waltz/common/dh.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
 )
 
@@ -9,7 +8,9 @@ import (
 Great RedHat docs on this subject:
 https://www.redhat.com/en/blog/understanding-and-verifying-security-diffie-hellman-parameters
 
+//g is a primitive root modulo
 g = 10
+//p is a shared prime
 p = 541
 
 a = 2
@@ -25,12 +26,19 @@ keyb = A^B mod p = 1004 mod 541 = 478
 func dh_handshake(conn *Net.connection, conn_type string) int {
 
 	//calculate my prime and shared prime
-	prime, err := rand.Prime(rand.Reader, 2048)
+	//myint is private, no more than 1mil, no less than 10thou
+	myint := math.rand.Intn(1000000) + 10000
+	fmt.Println(myint)
+
+	prime, err := crypto.rand.Prime(crypto.rand.Reader, 2048)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	//calculate shared int
+	//sharedint is public, no more than 1mil, no less than 10thou
+	sharedint := math.rand.Intn(1000000) + 10000
+	fmt.Println(sharedint)
 
 	//exchange values
 
