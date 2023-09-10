587ce16c6c6aa7cb9236f8b21eea47c657098ec3
Kayleigh Traister <kayleigh.traister@gmail.com>
2023-09-09 22:55:22 -0400

    recreated primes from ubuntu release, modified python script to spit out rn.go

--- endless_waltz/common/random_numbers/moduli.py
+++ endless_waltz/common/random_numbers/moduli.py
@@ -1,7 +1,13 @@
 #quick python program to convert ssh moduli files into useable generator/prime combos
 
+print("package main")
+print()
+print("var moduli_pairs = []string{")
+
 with open("outfile", 'r') as file:
     for line in file.readlines():
         moduli=line.split(' ')[6]
         res = int(moduli, 16)
-        print(str(line.split(' ')[5]), str(res))
+        print("        \"" + str(line.split(' ')[5]) + ":" + str(res) + "\",")
+
+print("}")
