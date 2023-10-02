3972025132c98e17da7d608019913000c70bf579
Kayleigh Traister <kayleigh.traister@gmail.com>
2023-10-02 14:44:22 -0400

    stubbed signup page handlers

--- endless_waltz/webapp/webapp.go
+++ endless_waltz/webapp/webapp.go
@@ -20,6 +20,41 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, string(home))
 }
 
+func signUpPageHandler(w http.ResponseWriter, r *http.Request) {
+	signUpForm, err := os.ReadFile("pages/signUp.html")
+	if err != nil { return }
+
+	fmt.Fprintln(w, string(signUpForm))
+
+}
+
+func signUpHandler(w http.ResponseWriter, req *http.Request) {
+	logger, ok := req.Context().Value("logger").(*logrus.Logger)
+	if !ok {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		logger.Error("Could not configure logger!")
+		return
+	}
+
+	// Parse form data
+	err := req.ParseForm()
+	if err != nil {
+		http.Error(w, "Failed to parse form data", http.StatusBadRequest)
+		return
+	}
+
+	// Get username and password from the form
+	username := req.FormValue("username")
+
+	//create our hasher to hash our pass
+	hash := sha512.New()
+	hash.Write([]byte(req.FormValue("password")))
+	hashSum := hash.Sum(nil)
+	password := hex.EncodeToString(hashSum)
+
+	//redirect to main page here pending email confirmation
+}
+
 func loginPageHandler(w http.ResponseWriter, r *http.Request) {
 	loginForm, err := os.ReadFile("pages/login.html")
 	if err != nil { return }
@@ -110,6 +145,8 @@ func main() {
 	router.HandleFunc("/", homePageHandler).Methods("GET")
 	router.HandleFunc("/login", loginPageHandler).Methods("GET")
 	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/signUp", signUpPageHandler).Methods("GET")
+	router.HandleFunc("/signUp", signUpHandler).Methods("POST")
 	router.HandleFunc("/protected", protectedPageHandler).Methods("GET")
 	http.ListenAndServe(":8080", router)
 }
