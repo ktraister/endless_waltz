629a96ecff0e833e5b9e8fc245d1abb73211524d
Kayleigh Traister <kayleigh.traister@gmail.com>
2023-10-02 13:28:20 -0400

    basic app works, did formatting

--- endless_waltz/webapp/webapp.go
+++ endless_waltz/webapp/webapp.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
-    "github.com/gorilla/sessions"
-    "github.com/sirupsen/logrus"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
 )
 
-	var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
-    // Implement your custom page logic here
-    fmt.Fprintln(w, "Welcome to MyWebApp!")
+	// Implement your custom page logic here
+	fmt.Fprintln(w, "Welcome to MyWebApp!")
 }
 
 func loginPageHandler(w http.ResponseWriter, r *http.Request) {
-    // Serve your login page (e.g., an HTML form)
-    // You can use HTML templates or generate the form directly here
-    // Example HTML form:
-    loginForm := `
+	// Serve your login page (e.g., an HTML form)
+	// You can use HTML templates or generate the form directly here
+	// Example HTML form:
+	loginForm := `
     <html>
     <body>
         <form method="POST" action="/login">
@@ -31,77 +33,91 @@ func loginPageHandler(w http.ResponseWriter, r *http.Request) {
     </body>
     </html>
     `
-    fmt.Fprintln(w, loginForm)
+	fmt.Fprintln(w, loginForm)
 }
 
 func loginHandler(w http.ResponseWriter, req *http.Request) {
-        logger, ok := req.Context().Value("logger").(*logrus.Logger)
-        if !ok {
-                http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-                logger.Error("Could not configure logger!")
-                return
-        }   
+	logger, ok := req.Context().Value("logger").(*logrus.Logger)
+	if !ok {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		logger.Error("Could not configure logger!")
+		return
+	}
 
-    // Parse form data
-    err := req.ParseForm()
-    if err != nil {
-        http.Error(w, "Failed to parse form data", http.StatusBadRequest)
-        return
-    }
+	// Parse form data
+	err := req.ParseForm()
+	if err != nil {
+		http.Error(w, "Failed to parse form data", http.StatusBadRequest)
+		return
+	}
 
-    // Get username and password from the form
-    username := req.FormValue("username")
-    password := req.FormValue("password")
+	// Get username and password from the form
+	username := req.FormValue("username")
 
-    // Authenticate user (you'll need to implement this function)
-    if checkAuth(username, password, logger) {
-        // Successful login
-        // Create a session for the user
-        session, _ := store.Get(req, "session-name")
-        session.Values["authenticated"] = true
-        session.Save(req, w)
+	//create our hasher to hash our pass
+	hash := sha512.New()
+	hash.Write([]byte(req.FormValue("password")))
+	hashSum := hash.Sum(nil)
+	password := hex.EncodeToString(hashSum)
 
-        // Redirect to a protected page or display a success message
-        http.Redirect(w, req, "/protected", http.StatusSeeOther)
-    } else {
-        // Failed login
-        // Display an error message or redirect to the login page
-        http.Redirect(w, req, "/login", http.StatusSeeOther)
-    }
+	// Authenticate user (you'll need to implement this function)
+	if checkAuth(username, password, logger) {
+		// Successful login
+		// Create a session for the user
+		session, _ := store.Get(req, "session-name")
+		session.Values["authenticated"] = true
+		session.Save(req, w)
+
+		// Redirect to a protected page or display a success message
+		http.Redirect(w, req, "/protected", http.StatusSeeOther)
+	} else {
+		// Failed login
+		// Display an error message or redirect to the login page
+		http.Redirect(w, req, "/login", http.StatusSeeOther)
+	}
 }
 
-func protectedPageHandler(w http.ResponseWriter, r *http.Request) {
-    session, err := store.Get(r, "session-name")
-    if err != nil {
-        http.Error(w, "Session not found", http.StatusUnauthorized)
-        return
-    }
+func protectedPageHandler(w http.ResponseWriter, req *http.Request) {
+	logger, ok := req.Context().Value("logger").(*logrus.Logger)
+	if !ok {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		logger.Error("Could not configure logger!")
+		return
+	}
 
-    // Check if the user is authenticated
-    if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-        http.Error(w, "Unauthorized", http.StatusUnauthorized)
-        return
-    }
+	session, err := store.Get(req, "session-name")
+	if err != nil {
+		logger.Error("sessionNotFound")
+		http.Error(w, "Session not found", http.StatusUnauthorized)
+		return
+	}
 
-    // Display your protected page content here
-    fmt.Fprintln(w, "Welcome to the Protected Page!")
+	// Check if the user is authenticated
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		logger.Error("Client unauthorized")
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	// Display your protected page content here
+	fmt.Fprintln(w, "Welcome to the Protected Page!")
 }
 
 func main() {
-        MongoURI = os.Getenv("MongoURI")
-        MongoUser = os.Getenv("MongoUser")
-        MongoPass = os.Getenv("MongoPass")
-        LogLevel := os.Getenv("LogLevel")
-        LogType := os.Getenv("LogType")
- 
-        logger := createLogger(LogLevel, LogType)
-        logger.Info("WebApp Server finished starting up!")
+	MongoURI = os.Getenv("MongoURI")
+	MongoUser = os.Getenv("MongoUser")
+	MongoPass = os.Getenv("MongoPass")
+	LogLevel := os.Getenv("LogLevel")
+	LogType := os.Getenv("LogType")
 
-        router := mux.NewRouter()
-        router.Use(LoggerMiddleware(logger))
-        router.HandleFunc("/", homePageHandler).Methods("GET") 
+	logger := createLogger(LogLevel, LogType)
+	logger.Info("WebApp Server finished starting up!")
+
+	router := mux.NewRouter()
+	router.Use(LoggerMiddleware(logger))
+	router.HandleFunc("/", homePageHandler).Methods("GET")
 	router.HandleFunc("/login", loginPageHandler).Methods("GET")
 	router.HandleFunc("/login", loginHandler).Methods("POST")
-	router.HandleFunc("/protected", loginHandler).Methods("POST")
-        http.ListenAndServe(":8080", router)
-}  
+	router.HandleFunc("/protected", protectedPageHandler).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
