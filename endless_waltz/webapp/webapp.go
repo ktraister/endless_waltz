c30b69e5c4165d806ffe3f8e1de01391ce4e81fa
Kayleigh Traister <kayleigh.traister@gmail.com>
2023-10-02 20:26:41 -0400

    added new service to docker & ci, changed webapp page file names and handlers

--- endless_waltz/webapp/webapp.go
+++ endless_waltz/webapp/webapp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
@@ -9,27 +10,48 @@ import (
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
 func imgHandler(w http.ResponseWriter, req *http.Request) {
 	img, err := os.ReadFile(fmt.Sprintf("pages%s", req.URL.Path))
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 	w.Header().Set("Content-Type", "image/png")
 	w.Write(img)
 }
 
+func staticHandler(w http.ResponseWriter, req *http.Request) {
+	img, err := os.ReadFile(fmt.Sprintf("pages%s", req.URL.Path))
+	if err != nil {
+		return
+	}
+	w.Write(img)
+}
+
+
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	// Implement your custom page logic here
-	home, err := os.ReadFile("pages/home.html")
-	if err != nil { return }
+	home, err := os.ReadFile("pages/home")
+	if err != nil {
+		return
+	}
 	fmt.Fprintln(w, string(home))
 }
 
 func signUpPageHandler(w http.ResponseWriter, r *http.Request) {
 	signUpForm, err := os.ReadFile("pages/signUp.html")
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 
 	fmt.Fprintln(w, string(signUpForm))
 
@@ -37,15 +59,15 @@ func signUpPageHandler(w http.ResponseWriter, r *http.Request) {
 
 func loginPageHandler(w http.ResponseWriter, r *http.Request) {
 	loginForm, err := os.ReadFile("pages/login.html")
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 
 	fmt.Fprintln(w, string(loginForm))
 
 }
 
-
 func signUpHandler(w http.ResponseWriter, req *http.Request) {
-    /*
 	logger, ok := req.Context().Value("logger").(*logrus.Logger)
 	if !ok {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -60,8 +82,43 @@ func signUpHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// Get username and password from the form
-	username := req.FormValue("username")
+	if req.FormValue("password") != req.FormValue("confirm_password") {
+		http.Error(w, "Password did not match confirmation", http.StatusBadRequest)
+		return
+	}
+
+	//setup database access
+	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
+	defer cancel()
+	credential := options.Credential{
+		Username: MongoUser,
+		Password: MongoPass,
+	}
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoURI).SetAuth(credential))
+	if err != nil {
+		logger.Fatal(err)
+		return
+	} else {
+		logger.Info("Database connection succesful!")
+	}
+	auth_db := client.Database("auth").Collection("keys")
+
+	//check database to ensure username/email ! already exists
+	filters := []primitive.M{bson.M{"User": req.FormValue("username")},
+		bson.M{"Email": req.FormValue("email")},
+	}
+
+	for _,filter := range filters {
+	        var result bson.M
+		fmt.Println(filter)
+		err := auth_db.FindOne(ctx, filter).Decode(&result)
+		if err != mongo.ErrNoDocuments {
+			//be more specific in future
+			http.Error(w, "Username or Email collision", http.StatusBadRequest)
+			logger.Error(err)
+			return
+		}
+	}
 
 	//create our hasher to hash our pass
 	hash := sha512.New()
@@ -69,8 +126,13 @@ func signUpHandler(w http.ResponseWriter, req *http.Request) {
 	hashSum := hash.Sum(nil)
 	password := hex.EncodeToString(hashSum)
 
+	//Write to database with information
+	_, err = auth_db.InsertOne(ctx, bson.M{"User":req.FormValue("username"),"Passwd":password,"Email":req.FormValue("email"),"Active":true})
+	if err != nil {
+		return
+	}
+
 	//redirect to main page here pending email confirmation
-	*/
 	http.Redirect(w, req, "/signUpSuccess", http.StatusSeeOther)
 
 }
@@ -155,10 +217,11 @@ func main() {
 	router := mux.NewRouter()
 	router.Use(LoggerMiddleware(logger))
 	router.HandleFunc("/", homePageHandler).Methods("GET")
+	router.HandleFunc("/error", staticHandler).Methods("GET")
 	router.HandleFunc("/img/{id}", imgHandler).Methods("GET")
-	router.HandleFunc("/login", loginPageHandler).Methods("GET")
+	router.HandleFunc("/login", staticHandler).Methods("GET")
 	router.HandleFunc("/login", loginHandler).Methods("POST")
-	router.HandleFunc("/signUp", signUpPageHandler).Methods("GET")
+	router.HandleFunc("/signUp", staticHandler).Methods("GET")
 	router.HandleFunc("/signUp", signUpHandler).Methods("POST")
 	router.HandleFunc("/protected", protectedPageHandler).Methods("GET")
 	http.ListenAndServe(":8080", router)
