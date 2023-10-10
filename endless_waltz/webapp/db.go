2438ac30dff10c0af7a0cd1cb0617ab70edaf247
Kayleigh Traister <kayleigh.traister@gmail.com>
2023-10-10 12:54:14 -0400

    password resets seem to be working, cautious optimisim (fingers crossed)

--- endless_waltz/webapp/db.go
+++ endless_waltz/webapp/db.go
@@ -45,6 +45,138 @@ func deleteUser(logger *logrus.Logger, user string) bool {
 	}
 }
 
+func prepareUserPassReset(logger *logrus.Logger, user string, token string) (string, error) {
+	//creating context to connect to mongo
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	credential := options.Credential{
+		Username: MongoUser,
+		Password: MongoPass,
+	}
+	//actually connect to mongo
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoURI).SetAuth(credential))
+	if err != nil {
+		logger.Error("Could not connect to mongo:", err)
+		return "", err
+	}
+
+	auth_db := client.Database("auth").Collection("keys")
+
+	// Check if the item exists in the collection
+	logger.Debug(fmt.Sprintf("checking email for user '%s'", user))
+	filter := bson.M{"User": user}
+	var result bson.M
+	err = auth_db.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		logger.Error("Generic mongo read error: ", err)
+		return "", err
+	}
+
+	//if the user exists, let's set their reset token and time
+	update := bson.D{{"$set", bson.D{{"passwordResetToken", token}, {"passwordResetTime", time.Now().Unix()}}}}
+	_, err = auth_db.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		logger.Error("Error setting user password reset data: ", err)
+		return "", err
+	}
+
+	return result["Email"].(string), nil
+}
+
+func verifyPasswordReset(logger *logrus.Logger, email string, user string, token string) bool {
+	//creating context to connect to mongo
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	credential := options.Credential{
+		Username: MongoUser,
+		Password: MongoPass,
+	}
+	//actually connect to mongo
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoURI).SetAuth(credential))
+	if err != nil {
+		logger.Error("verifyUserSignup mongo generic error ", err)
+		return false
+	}
+
+	auth_db := client.Database("auth").Collection("keys")
+
+	// Check if the item exists in the collection
+	logger.Debug(fmt.Sprintf("resetting user pass for '%s'", user))
+	filter := bson.M{"User": user, "Email": email, "passwordResetToken": token}
+	var result bson.M
+	err = auth_db.FindOne(context.TODO(), filter).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		logger.Warn("No creds found, unauthorized")
+		return false
+	} else if err != nil {
+		logger.Error("Generic mongo FindOne error: ", err)
+		return false
+	}
+
+	regTime := int(result["passwordResetTime"].(int64))
+
+	threshold := regTime + 600
+
+	//check the result for appropriate values
+	if int(time.Now().Unix()) > threshold {
+		logger.Warn(fmt.Sprintf("User '%s' exceeded password reset threshold", user))
+		return false
+	}
+
+	return true
+}
+
+func submitPasswordReset(logger *logrus.Logger, email string, user string, token string, passHash string) bool {
+	//creating context to connect to mongo
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	credential := options.Credential{
+		Username: MongoUser,
+		Password: MongoPass,
+	}
+	//actually connect to mongo
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoURI).SetAuth(credential))
+	if err != nil {
+		logger.Error("verifyUserSignup mongo generic error ", err)
+		return false
+	}
+
+	auth_db := client.Database("auth").Collection("keys")
+
+	// Check if the item exists in the collection
+	logger.Debug(fmt.Sprintf("resetting user pass for '%s'", user))
+	filter := bson.M{"User": user, "Email": email, "passwordResetToken": token}
+	var result bson.M
+	err = auth_db.FindOne(context.TODO(), filter).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		logger.Warn("No creds found, unauthorized")
+		return false
+	} else if err != nil {
+		logger.Error("Generic mongo FindOne error: ", err)
+		return false
+	}
+
+	regTime := int(result["passwordResetTime"].(int64))
+
+	threshold := regTime + 600
+
+	//check the result for appropriate values
+	if int(time.Now().Unix()) > threshold {
+		logger.Warn(fmt.Sprintf("User '%s' exceeded password reset threshold", user))
+		return false
+	}
+
+	//if the user exists, let's set their reset token and time
+	update := bson.D{{"$set", bson.D{{"Password", passHash}, {"passwordResetToken", nil}, {"passwordResetTime", nil}}}}
+	_, err = auth_db.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		logger.Error("Error setting user password reset data: ", err)
+		return false
+	}
+
+	return true
+}
+
 func verifyUserSignup(logger *logrus.Logger, email string, user string, token string) bool {
 	//creating context to connect to mongo
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -74,8 +206,6 @@ func verifyUserSignup(logger *logrus.Logger, email string, user string, token st
 		logger.Error("Generic mongo FindOne error: ", err)
 		return false
 	}
-
-	logger.Error(result)
 
 	regTime, err := strconv.Atoi(result["SignupTime"].(string))
 	if err != nil {
--- endless_waltz/webapp/email.go
+++ endless_waltz/webapp/email.go
@@ -17,7 +17,52 @@ func sendVerifyEmail(logger *logrus.Logger, username string, targetUser string, 
 	auth := smtp.PlainAuth("", emailUser, emailPass, "smtp.gmail.com")
 
 	//grab our template
-	fileContent, err := ioutil.ReadFile("./pages/email/template")
+	fileContent, err := ioutil.ReadFile("./pages/email/verifyTemplate")
+	if err != nil {
+		log.Fatal("Unable to read email template")
+		return err
+	}
+
+	formHost := "https://endlesswaltz.xyz"
+	if os.Getenv("ENV") == "local" {
+		formHost = "http://localhost:8080"
+	}
+
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	from := emailUser
+	to := []string{targetUser}
+	msg := []byte(fmt.Sprintf("To: %s\r\n", targetUser) +
+		"Subject: Welcome to Endless Waltz\r\n" +
+		mime +
+		fmt.Sprintf(string(fileContent), formHost, username, targetUser, token))
+
+	err = smtp.SendMail("smtp.gmail.com:587", auth, from, to, msg)
+
+	if err != nil {
+		log.Fatal("unable to send email to gmail server")
+		return err
+	}
+
+	return nil
+}
+
+func sendResetEmail(logger *logrus.Logger, username string, token string) error {
+	//query the db for the email corresponding to the username
+	//and, yknow, make sure the request isn't a bogus user
+	targetUser, err := prepareUserPassReset(logger, username, token)
+	if err != nil {
+		logger.Warn("bogus reset: ", err)
+		return err
+	}
+
+	emailUser := os.Getenv("EmailUser")
+	emailPass := os.Getenv("EmailPass")
+
+	//connect to our server
+	auth := smtp.PlainAuth("", emailUser, emailPass, "smtp.gmail.com")
+
+	//grab our template
+	fileContent, err := ioutil.ReadFile("./pages/email/resetTemplate")
 	if err != nil {
 		log.Fatal("Unable to read email template")
 		return err
